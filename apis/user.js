a247a88d193c1d64cda387705baebd8827950567
朱晓波 <1726258155@qq.com>
2020-06-08 13:57:27 +0800

    add

---
+++ apis/user.js
@@ -0,0 +1,15 @@
+import {
+  httpRequest
+} from '@/utils'
+
+/**
+ * @function 账号密码登录
+ */
+export const loginByPwd = () => {
+  return new Promise((resolve, reject) => {
+    resolve({
+      code: 200,
+      data: null
+    })
+  })
+}
