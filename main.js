ac328783a724143fb13398f90ff766832df81c9f
朱晓波 <1726258155@qq.com>
2020-07-17 14:29:07 +0800

    ad

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import * as R from 'ramda';
 import validator from 'validator';
-import animated from 'animate.css';
 import { tool } from '@/utils';
 import MyPlugin from '@/plugin';
 // #ifdef H5
+import animated from 'animate.css';
 import * as dd from 'dingtalk-jsapi';
 import store from './store';
 import App from './App';
