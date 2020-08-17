abdd39e941befa25fb7eca854d16fc9e0f1461ad
朱晓波 <1726258155@qq.com>
2020-08-17 14:04:28 +0800

    add

--- wxcomponents/vant-weapp/calendar/calendar.vue
+++ wxcomponents/vant-weapp/calendar/calendar.vue
@@ -6,7 +6,7 @@
     </header>
 
     <scroll-view class="van-calendar__body" scroll-y :scroll-into-view="scrollIntoView">
-      <month v-for="(item,index) in (computed.getMonths(minDate, maxDate))" :key="item.index" :id="'month'+(index)" class="month" :data-date="item" :date="item" :type="type" :color="color" :minDate="minDate" :maxDate="maxDate" :showMark="showMark" :formatter="formatter" :rowHeight="rowHeight" :currentDate="currentDate" :showSubtitle="showSubtitle" :allowSameDay="allowSameDay" :showMonthTitle="index !== 0 || !showSubtitle" @click="_$self.$parent.$parent[('onClickDay')]($event)"></month>
+      <month v-for="(item,index) in (computed.getMonths(minDate, maxDate))" :key="item.index" :id="'month'+(index)" class="month" :data-date="item" :date="item" :type="type" :color="color" :minDate="minDate" :maxDate="maxDate" :showMark="showMark" :formatter="formatter" :rowHeight="rowHeight" :currentDate="currentDate" :showSubtitle="showSubtitle" :allowSameDay="allowSameDay" :showMonthTitle="index !== 0 || !showSubtitle" @click="_$self.$parent[('onClickDay')]($event)"></month>
     </scroll-view>
 
     <view :class="'van-calendar__footer '+(safeAreaInsetBottom ? 'van-calendar__footer--safe-area-inset-bottom' : '')">
@@ -14,7 +14,7 @@
     </view>
 
     <view :class="'van-calendar__footer '+(safeAreaInsetBottom ? 'van-calendar__footer--safe-area-inset-bottom' : '')">
-      <van-button v-if="showConfirm" round block type="danger" :color="color" custom-class="van-calendar__confirm" :disabled="computed.getButtonDisabled(type, currentDate)" nativeType="text" @click="_$self.$parent.$parent[('onConfirm')]($event)">
+      <van-button v-if="showConfirm" round block type="danger" :color="color" custom-class="van-calendar__confirm" :disabled="computed.getButtonDisabled(type, currentDate)" nativeType="text" @click="_$self.$parent[('onConfirm')]($event)">
         {{ computed.getButtonDisabled(type, currentDate) ? confirmDisabledText : confirmText }}
       </van-button>
     </view>
